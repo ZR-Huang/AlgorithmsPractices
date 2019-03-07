a4edbf9fd5b50c15c9e0eb67a9c8c979befa2870
Roger's-Mac <hzr951110@gmail.com>
2019-03-07 13:21:49 +0800

    optimize 929

--- leetcode/5. Unique Email Addresses(929)[Easy].py
+++ leetcode/5. Unique Email Addresses(929)[Easy].py
@@ -1,13 +1,24 @@
 test_input = ["test.email+alex@leetcode.com","test.e.mail+bob.cathy@leetcode.com","testemail+david@lee.tcode.com"]
 
+def time(func):
+    def wrapper(*args, **kw):
+        import time
+        start = time.time()
+        for _ in range(10):
+            func(*args, **kw)
+        costTime = (time.time()-start)/10
+        print(costTime)
+        return func(*args, **kw)
+    return wrapper
+
 class Solution:
     def parseLocalName(self, localName):
         pos = localName.find('+')
         localName = localName[:pos] # strip string
-        splits = localName.split('.')
-        lastLocalName = "".join(splits)
+        lastLocalName = "".join(localName.split('.'))
         return lastLocalName
 
+    @time
     def numUniqueEmails(self, emails) -> int:
         # print(emails)
         receivedEmails = set()
@@ -21,7 +32,40 @@ class Solution:
         return len(receivedEmails)
 
 
-solver = Solution()
-print(solver.numUniqueEmails(test_input))
+class SolutionV2:
+    @time
+    def numUniqueEmails(self, emails) -> int:
+        receivedEmails = dict()
+        for email in emails:
+            parsedEmail = str()
+            localName, domainName = email.split('@')
+            for c in localName:
+                if c == '.':
+                    continue
+                elif c == '+':
+                    break
+                else:
+                    parsedEmail += c
+            parsedEmail += "@" + domainName
+            if parsedEmail not in receivedEmails:
+                receivedEmails[parsedEmail] = True
+        return len(receivedEmails)
 
 
+class Answer:
+    @time
+    def numUniqueEmails(self, emails):
+        seen = set()
+        for email in emails:
+            local, domain = email.split('@')
+            if '+' in local:
+                local = local[:local.index('+')]
+            seen.add(local.replace('.','') + '@' + domain)
+        return len(seen)
+
+solver1 = Solution()
+solver2 = SolutionV2()
+solver3 = Answer()
+print(solver1.numUniqueEmails(test_input))
+print(solver2.numUniqueEmails(test_input))
+print(solver3.numUniqueEmails(test_input))
