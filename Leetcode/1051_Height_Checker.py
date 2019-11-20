3b00fa95384890e058fdb69a9940f975f4a8ba88
Zhangrong Huang <hzr951110@gmail.com>
2019-11-20 21:13:13 +0800

    .

--- Leetcode/1051_Height_Checker.py
+++ Leetcode/1051_Height_Checker.py
@@ -1,13 +1,46 @@
 class Solution:
     def heightChecker(self, heights):
-        
-        def merge_sort(start, end):
-            if start == end:
-                return 
-            
-            mid = int((start + end)/2)
+        '''
+        sort the heights array and compare the new and the old.
+        '''
+        # sorted_heights = sorted(heights)
+        old_heights = heights[:]
+        self.heap_sort(heights)
+        # print(sorted_heights)
+        result = 0 
+        for old, new in zip(old_heights, heights):
+            if old != new:
+                result += 1
+        return result
 
-            merge_sort(start, mid)
-            merge_sort(mid+1, end) 
+
+    def heap_sort(self, array):
+        for i in range(int((len(array)-2)/2), -1, -1):
+            self.down_adjust(array, i, len(array))
+
+        for i in range(len(array)-1, 0, -1):
+            array[i], array[0] = array[0], array[i]
+            self.down_adjust(array, 0, i)
+        
+
+    def down_adjust(self, array, parent_index, length):
+        temp = array[parent_index]
+
+        child_index = 2*parent_index + 1
+
+        while child_index < length:
+            if child_index + 1< length and array[child_index+1] > array[child_index]:
+                child_index += 1
             
+            if temp > array[child_index]:
+                break
+            
+            array[parent_index] = array[child_index]
+            parent_index = child_index
+            child_index = 2*child_index + 1
+        
+        array[parent_index] = temp
+
+
+print(Solution().heightChecker([1,1,4,2,1,3]))
             
