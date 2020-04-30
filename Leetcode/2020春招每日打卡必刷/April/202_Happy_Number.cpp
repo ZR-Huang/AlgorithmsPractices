f137ee15afa15ce52d011df81cca35cb4c4587e8
Roger <zr.huang0@gmail.com>
2020-04-30 10:47:04 +0800

    Add

---
+++ Leetcode/2020春招每日打卡必刷/April/202_Happy_Number.cpp
@@ -0,0 +1,45 @@
+/*
+Write an algorithm to determine if a number n is "happy".
+
+A happy number is a number defined by the following process: 
+Starting with any positive integer, 
+replace the number by the sum of the squares of its digits, 
+and repeat the process until the number equals 1 (where it will stay), 
+or it loops endlessly in a cycle which does not include 1. 
+Those numbers for which this process ends in 1 are happy numbers.
+
+Return True if n is a happy number, and False if not.
+
+Example: 
+Input: 19
+Output: true
+Explanation: 
+1^2 + 9^2 = 82
+8^2 + 2^2 = 68
+6^2 + 8^2 = 100
+1^2 + 0^2 + 0^2 = 1
+*/
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> visited;
+        visited.insert(n);
+        while (n != 1)
+        {
+            int sum = 0;
+            while(n!=0){
+                int tmp = n % 10;
+                sum += tmp * tmp;
+                n /= 10;
+            }
+            n = sum;
+            if(visited.find(n)!=visited.end()) return false;
+            visited.insert(n);
+        }
+        return true;
+    }
+};
