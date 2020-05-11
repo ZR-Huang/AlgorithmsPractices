79d147977d13dc9c1eda1827d0961cd4d42208fd
Roger <zr.huang0@gmail.com>
2020-05-11 11:29:20 +0800

    Add

---
+++ Leetcode/2020春招每日打卡必刷/May/50_Pow_x_n_.cpp
@@ -0,0 +1,39 @@
+/*
+Implement pow(x, n), which calculates x raised to the power n (x^n).
+
+Example 1:
+
+Input: 2.00000, 10
+Output: 1024.00000
+Example 2:
+
+Input: 2.10000, 3
+Output: 9.26100
+Example 3:
+
+Input: 2.00000, -2
+Output: 0.25000
+Explanation: 2^-2 = 1/2^2 = 1/4 = 0.25
+
+Note:
+-100.0 < x < 100.0
+n is a 32-bit signed integer, within the range [−2^31, 2^31 − 1]
+*/
+
+class Solution {
+public:
+    double quick_mul(double x, long long N) {
+        double res = 1.0;
+        while(N > 0){
+            if (N & 1)
+                res *= x;
+            x *= x;
+            N >>=1;
+        }
+        return res;
+    }
+    double myPow(double x, int n) {
+        long long N = n;
+        return N >= 0 ? quick_mul(x, N) : quick_mul(1/x, -N);
+    }
+};
